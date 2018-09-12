8f41835e5b98b89ae09fb7b771950c9f731b4a5e
jphearn2 <jphearn2@gmail.com>
2018-09-12 15:36:15 -0500

    start of server

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	g++ -o server serverFolder/server.cpp
+
+clean:
+	rm server
---
+++ serverFolder/server.cpp
@@ -0,0 +1,12 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout << "Greetings Master\n";
+
+  
+
+  return 0;
+}
